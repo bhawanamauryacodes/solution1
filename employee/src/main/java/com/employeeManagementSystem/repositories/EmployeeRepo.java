cdc005a4fbd9053430fa105d2a24e59173691d11
Bhawna Maurya <91850085+bhawanamauryacodes@users.noreply.github.com>
2023-12-09 18:58:30 +0530

    Delete employee directory

--- employee/src/main/java/com/employeeManagementSystem/repositories/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.employeeManagementSystem.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.employeeManagementSystem.entities.Employee;
-
-public interface EmployeeRepo extends JpaRepository<Employee, Integer>{
-          
-}
